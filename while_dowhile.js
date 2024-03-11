acd528891553ef9c960dd11be74847a4c02ff324
Juni0rFerreira <junioredilsonferreira@hotmail.com>
2024-03-11 18:49:16 -0300

    commit

---
+++ while_dowhile.js
@@ -0,0 +1,19 @@
+// let i = 0;
+// while(i<10) {
+//     console.log(i);
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i);
+    i++;
+} while(i<10);
+
+//Diferença dos dois é que o do-while voce tem a garantia de execução de pelomenos
+//1x do bloco de comando do loop
+
+//While - Executa o loop enquanto a condição for verdadeira.
+
+//Do-While - Executa o loop enquanto a condição for verdadeira, 
+//porém você pode instanciar as variaveis contadoras dentro da estrutura do loop.
