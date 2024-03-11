9132ed2a39f2db4c8dae30b49e615e22dec55bc8
Juni0rFerreira <junioredilsonferreira@hotmail.com>
2024-03-11 19:31:46 -0300

    commit

--- vetores_matrizes.js
+++ vetores_matrizes.js
@@ -1,12 +1,9 @@
 var times = ['Corinthians', 'São Paulo', 'Palmeiras', 'Santos'];
-//               [0]            [1]          [2]         [3]
 
-console.log(times[1]); //retorna a informação desejada
-console.log(times.length); //retorna o tamanho
-console.log(times); //retorna o tamanho e os dados do vetor
-console.table(times); //retorna tudo em table para uma maior e melhor visualização
-
-//Alterar dentro da variavel
+console.log(times[1]); 
+console.log(times.length); 
+console.log(times);
+console.table(times); 
 
 times[2] = 'Flamengo';
-console.table(times); //Ele vai retornar com [2] alterado para Flamengo
+console.table(times); 
