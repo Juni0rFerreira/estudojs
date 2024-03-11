9668afe0a6f84c1adab6b9ef541ac1640901ec2a
Juni0rFerreira <junioredilsonferreira@hotmail.com>
2024-03-11 19:37:36 -0300

    commit

--- vetores_matrizes.js
+++ vetores_matrizes.js
@@ -1,4 +1,8 @@
 var times = ['Corinthians', 'São Paulo', 'Palmeiras', 'Santos'];
+var frutas = [
+    ['Abacaxi', 'Morango'],
+    ['Melancia', 'Maça']
+];
 
 console.log(times[1]); 
 console.log(times.length); 
@@ -7,3 +11,10 @@ console.table(times);
 
 times[2] = 'Flamengo';
 console.table(times); 
+
+//bidimensional
+
+console.log(frutas[2]);
+console.log(frutas.length);
+console.log(frutas);
+console.table(frutas);
