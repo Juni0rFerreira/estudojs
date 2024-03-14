fc747c70b243745c489e6abeffe451da912fe73b
JuniorFerreira <junioredilsonferreira@hotmail.com>
2024-03-14 16:33:54 -0300

    commit

--- for.js
+++ for.js
@@ -1,4 +1,14 @@
-for (var i = 5; i <= 100; i++) {
-    console.log(i);
+// for (var i = 5; i <= 100; i++) {
+//     console.log(i);
     
-  }
+//   }
+
+// for (let i=50; i >=0; i--) {
+//   console.log(i);
+// }
+
+var array = ['banana', 'pera', 'maca'];
+
+for (let frutas=0; frutas < array.length; frutas++) {
+  console.log(array[frutas]);
+}
--- vetores_matrizes.js
+++ vetores_matrizes.js
@@ -1,9 +1,17 @@
-var times = ['Corinthians', 'São Paulo', 'Palmeiras', 'Santos'];
+// var times = ['Corinthians', 'São Paulo', 'Palmeiras', 'Santos'];
 var frutas = [
     ['Abacaxi', 'Morango'],
     ['Melancia', 'Maça'],
     ['teste', 'testando2']
 ];
+
+for ( var i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
+
+// console.log(frutas[0].length);
+// console.log(frutas[1].length);
+// console.log(frutas[2].length);
 
 // console.log(times[1]); 
 // console.log(times.length); 
@@ -15,7 +23,8 @@ var frutas = [
 
 // //bidimensional
 
-console.log(frutas[2]);
-console.log(frutas.length, frutas);
-console.log(frutas);
-console.table(frutas);
+// console.log(frutas[2]);
+// console.log(frutas.length, frutas);
+// console.log(frutas);
+// console.table(frutas);
+
