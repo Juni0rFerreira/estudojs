5bb11cf899d6f04eff72471e0cc0f01159f62b87
Juni0rFerreira <junioredilsonferreira@hotmail.com>
2024-03-14 17:42:01 -0300

    commit

--- lacos_repeticao.js
+++ lacos_repeticao.js
@@ -18,22 +18,32 @@
 //Do-While - Executa o loop enquanto a condição for verdadeira, 
 //porém você pode instanciar as variaveis contadoras dentro da estrutura do loop.
 
-for(let i=10;i>=1;i--){
-    console.log(i);
+// for(let i=10;i >= 1;i--){
+//     console.log(i);
+// }
+
+
+// ['a', 'b'].forEach(function(item) {
+//     console.log(item);
+// });
+
+// const array = ['a', 'b', 'c'];
+// array.forEach(function(item) {
+//     console.log(item);
+// });
+
+let array = ['joao', 'pedro', 'bruna', 'junior'];
+
+for (let i=0; i<=array.length; i++) {
+    console.log(array[i]);
 }
 
+// for(let i=10;i >= 1;i--){
+//     console.log(i);
+// }
 
-['a', 'b'].forEach(function(item) {
-    console.log(item);
-});
-
-const array = ['a', 'b', 'c'];
-array.forEach(function(item) {
-    console.log(item);
-});
-
-
-
-
+// for (let i=0; i<=10; i++) {
+//     console.log(i);
+// }
 
 
